5cc49c6f58e689b5d1034fac9f5e438b34da5b2f
callmenikk <formyauto75@gmail.com>
2021-12-08 01:04:06 +0400

    fixed bug, ended project

--- src/Components/Playground.tsx
+++ src/Components/Playground.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, FC, useRef, KeyboardEvent } from "react";
+import { useState, useEffect, FC, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import "../style/playground.css";
 
@@ -16,56 +16,56 @@ const Playground = () => {
   const [start, setStart] = useState<number>(3);
   const [timer, setTimer] = useState<number>(0);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [countWrong, setCountWrong] = useState<number>(0)
+  const [countWrong, setCountWrong] = useState<number>(0);
   const [sentence, setSentence] = useState<quotesProps>({
     text: "",
     author: "",
   });
   let history = useHistory();
 
-  console.log(timer)
 
   useEffect(() => {
     if (
       currentIndex === sentence.text.length - 1 &&
-      !(typeRef!.current?.children[currentIndex].classList.contains("wrong"))
-    ){
-    localStorage.setItem('timer', timer.toString())
-        localStorage.setItem('sentence-info', JSON.stringify(sentence))
-        localStorage.setItem('count-wrong', countWrong.toString())
+      !typeRef!.current?.children[currentIndex].classList.contains("wrong")
+    ) {
+      localStorage.setItem("timer", (timer - 3).toString());
+      localStorage.setItem("sentence-info", JSON.stringify(sentence));
+      localStorage.setItem("count-wrong", countWrong.toString());
       history.push("/results");
     }
-    }, [currentIndex]);
+  }, [currentIndex]);
 
   useEffect(() => {
-    if (start !== 0) return;
-
     const interval = setInterval(() => {
-        setTimer(timer + 1)
-    }, 1000)
+      setTimer(timer + 1);
+    }, 1000);
 
-    return () => clearInterval(interval)
-    })
+    return () => clearInterval(interval);
+  }, [timer]);
 
   const keyBind = (e: any) => {
     //if keyBind is correct and last letter isnt typed wrong, go to next step
-    if (
+    if(e.keyCode === 16) return
+    else if (
       e.key === sentence.text[currentIndex] &&
       !typeRef!.current?.children[currentIndex].classList.contains("wrong")
     ) {
-    
-        setCurrentIndex(currentIndex + 1);
-        typeRef!.current?.children[currentIndex].classList.add("correct");
-     
-        //if the typed is wrong and also that's not a backspace then make it red
-    } else if (e.key !== sentence.text[currentIndex] && e.key !== "Backspace") {
+      setCurrentIndex(currentIndex + 1);
+      typeRef!.current?.children[currentIndex].classList.add("correct");
+
+      //if the typed is wrong and also that's not a backspace then make it red
+    } else if (
+      e.key !== sentence.text[currentIndex] &&
+      e.key !== "Backspace" &&
+      !typeRef!.current?.children[currentIndex].classList.contains("wrong")
+    ) {
 
       typeRef!.current?.children[currentIndex].classList.add("wrong");
-        setCountWrong(countWrong + 1)
-
+      setCountWrong(countWrong + 1);
+  
     } else if (e.key === "Backspace" && currentIndex >= 0) {
-    
-       //if user clicks on backspace and index is bigger or equals 0 remove "wrong"
+      //if user clicks on backspace and index is bigger or equals 0 remove "wrong"
       typeRef!.current?.children[currentIndex].classList.remove("wrong");
     }
   };
--- src/Components/Results.tsx
+++ src/Components/Results.tsx
@@ -1,7 +1,42 @@
+import { parse } from "path"
+import { useEffect, useState } from "react"
+import '../style/results.css'
+
+const defaultState = {
+    timer: 0,
+    sentence: {
+        author: "",
+        text: ""
+    }, 
+    mistakes: 0
+}
+
+
 const Results = () => {
+    const [results, setResults] = useState(defaultState)
+
+    useEffect(() => {
+        const timer = JSON.parse(localStorage.getItem("timer")!)
+        const sentence = JSON.parse(localStorage.getItem("sentence-info")!)
+        const mistakes = JSON.parse(localStorage.getItem("count-wrong")!)
+        
+        setResults({
+            timer: timer,
+            sentence: sentence,
+            mistakes: mistakes
+        })
+    }, [])
+
+
     return (
-        <div>
-            
+        <div className="results-container">
+            <main>
+                <p className="wpm">WPM: {Math.floor((results.sentence.text.length / 5) / (results.timer) * 60)}</p> 
+                <p className="mistakes" style={{color: "#ff4c4c"}}>Mistakes were made: {results.mistakes}</p>
+                <br />
+                <p className="quote">You typed {results.sentence.author}'s quote</p>
+                <p style={{fontSize: "1.2rem"}}>{results.sentence.text}</p>
+            </main>
         </div>
     )
 }
